f0e6c1edc7d0de3fa22536b7be34c6a50d5291b1
morris-huang-15 <64579717+morris-huang-15@users.noreply.github.com>
2022-04-29 02:47:09 +1000

    Added basic functions
    1.game object can be controlled by gesture
    2.basic UI

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
 
-
 var cvs = document.getElementById("gameBackground");
 var ctx = cvs.getContext('2d');
 
+var spaceship = new Image();
+var bg = new Image();
+var ad = new Image();
+spaceship.src = "spaceship.png";
+bg.src = "background.png";
+ad.src = "asteroid.png";
+
+var adCollection = [];
+adCollection[0] = {
+  x:30,
+  y:0
+}
 
 
+//var ad_start_pos_y = 0;
 
-
-
-// Use the hand with defaults (and show the webcam with wireframes)
 handsfree = new Handsfree({
-    showDebug: true,
-    hands: true
-  })
-
-  // Start webcam and tracking (personally, I always like to ask first)
-  handsfree.start()
-
-  //
-  document.addEventListener('handsfree-data', event => {
-  document.getElementById("p1").innerHTML = handsfree.data.hands.multiHandLandmarks[0][0].x;
-
+  showDebug: true,
+  hands: true
 })
+
+
+handsfree.start()
+
+
+document.addEventListener('handsfree-data', event => {
+document.getElementById("p1").innerHTML = handsfree.data.hands.multiHandLandmarks[0][0].x;
+})
+
+drawBackground();
+//draw();
+
+
+function drawBackground()
+{
+  ctx.imageSmoothingEnabled = false;
+  ctx.drawImage(bg,0,0,300,150);
+  requestAnimationFrame(drawBackground);
+}
+
+function draw (){
+  
+   ctx.imageSmoothingEnabled = false;
+
+   for (var i = 0; i < adCollection.length; i++){
+
+  ctx.drawImage(ad,30,adCollection[i].y,25,25);
+  adCollection[i].y++;
+  
+  }
+  document.addEventListener('handsfree-data', event => {
+  
+  ctx.drawImage(spaceship,250*handsfree.data.hands.multiHandLandmarks[0][0].x,100,15,20);
+  })
+  requestAnimationFrame(draw);
+  }
+  
+  
+  
+ 
 
 
